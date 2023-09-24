9ca75c9e6b49a54463b50ddaff3fca134f048983
Harshit Kumar Singh <110082422+harshitsinghcode@users.noreply.github.com>
2023-09-24 17:28:23 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Falcon7",
+  title: "Always Onwards and Upwards!",
+  email: "harshitsinghcode@gmail.com",
+  gitHub: "harshitsinghcode",
+  instagram: "harshitsingh.jpg",
+  linkedIn: "harshitkumarsingh25",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "iamharshitsing",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,15 +16,15 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/one(2).jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "falcon7";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Always Onwards and Upwards!";
 
 /**
  * List of some of skills or technologies you work on, are learning,
