35fa4ad4c3b83473026e04e41c587f3dc3dd0823
Harshit Kumar Singh <110082422+harshitsinghcode@users.noreply.github.com>
2023-09-24 17:39:06 +0530

    init_files

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/one(2).jpg";
+import image from "../images/one (2).jpg";
 
 const imageAltText = "falcon7";
 
